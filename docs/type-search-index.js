97e698cb3d740b520e18f9629ff64e891d6968dc
aluisamonteiro <150847379+aluisamonteiro@users.noreply.github.com>
2025-01-02 18:28:26 -0300

    Add files via upload

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"model","l":"Autor"},{"p":"controller","l":"AutorController"},{"p":"view","l":"AutorGUI"},{"p":"controller","l":"LoginController"},{"p":"view","l":"LoginGUI"},{"p":"model","l":"Usuario"}];updateSearchResults();
